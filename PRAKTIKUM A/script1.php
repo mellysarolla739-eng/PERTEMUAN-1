dad900c1c10321baf4c94082a710173ac1c21c7d
mellysarolla <mellysarolla739@gmail.com>
2025-12-08 13:22:13 +0700

    Fix folder and add PHP scripts

--- PRAKTIKUM A/script1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hargaProduk1 = 100000;
-$total1 = $hargaProduk1 + ($hargaProduk1 * 0.11);
-$hargaProduk2 = 200000;
-$total2 = $hargaProduk2 + ($hargaProduk2 * 0.11);
-echo "Total1: $total1\n";
-echo "Total2: $total2\n";
--- PRAKTIKUM A/script2.php
+++
@@ -1,4 +0,0 @@
-<?php
-$hargaProduk3 = 50000;
-$total3 = $hargaProduk3 + ($hargaProduk3 * 0.11);
-echo "Total3: $total3\n";
